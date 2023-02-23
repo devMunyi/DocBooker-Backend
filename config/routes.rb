e9e5ae847a33ee403609a1092525484cc7b64bfe
j-tee <juliustetteh@gmail.com>
2023-02-23 13:30:36 +0000

    COnfigure routes
    Signed-off-by: j-tee <juliustetteh@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    root to: 'home#index'
+    resources :users, only: [:index, :new, :create] do
+      resources :doctors, only: [:index, :new, :create] do
+        resources :reservations, only: [:index, :new, :create] 
+      end
+    end
+    resources :home, only: [:index]
+  end
 end
