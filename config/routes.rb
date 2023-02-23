4283996f56e3294424cc4bb98297635238802375
j-tee <juliustetteh@gmail.com>
2023-02-23 14:19:38 +0000

    Remove home#index from route.rb
    Signed-off-by: j-tee <juliustetteh@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   namespace :api do
-    root to: 'home#index'
     resources :users, only: [:index, :new, :create] do
       resources :doctors, only: [:index, :new, :create] do
         resources :reservations, only: [:index, :new, :create] 
