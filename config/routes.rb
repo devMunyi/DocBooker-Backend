bfa2c608df92a1eb627d2679428c5094aad85503
salimabdulai <maouslayer@gmail.com>
2023-03-06 13:52:02 +0000

    Add delete route to doctors route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     resources :users, only: [:index, :new, :create, :show] do
-      resources :doctors, only: [:index, :new, :create, :show] do
+      resources :doctors, only: [:index, :new, :create, :show, :destroy] do
         resources :reservations, only: [:index, :new, :create, :show, :destroy]   
       end
     end
