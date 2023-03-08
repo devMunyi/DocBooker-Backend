a8722e2472ac12f9bba84e5e5666ff1caee77fb4
salimabdulai <maouslayer@gmail.com>
2023-03-08 09:36:02 +0000

    Add initial swagger for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   namespace :api do
     resources :users, only: [:index, :new, :create, :show] do
       resources :doctors, only: [:index, :new, :create, :show, :destroy] do
