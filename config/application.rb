131c5a28481814979d39fe786b1f1d20c64ecd58
salimabdulai <maouslayer@gmail.com>
2023-03-10 09:00:16 +0000

    Add deploy settings

--- config/application.rb
+++ config/application.rb
@@ -13,11 +13,11 @@ require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
 # require "rails/test_unit/railtie"
+require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-# Bundler.require(*Rails.groups)
-Bundler.require(:default, :assets, Rails.env)
+Bundler.require(*Rails.groups)
 
 module BookingApp
   class Application < Rails::Application
